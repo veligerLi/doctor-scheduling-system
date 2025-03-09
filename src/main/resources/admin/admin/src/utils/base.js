bb8fbff04b11ed86c6ff2baf059c56c6e051ffe1
veliger <veliger@163.com>
2025-03-09 10:23:50 +0800

    更新跳转地址

--- src/main/resources/admin/admin/src/utils/base.js
+++ src/main/resources/admin/admin/src/utils/base.js
@@ -1,16 +1,16 @@
 const base = {
     get() {
         return {
-            url : "http://localhost:8080/springbootjf5zc/",
+            url: "https://doctor-scheduling-system-production.up.railway.app/springbootjf5zc/",
             name: "springbootjf5zc",
             // 退出到首页链接
-            indexUrl: 'http://localhost:8080/springbootjf5zc/front/index.html'
+            indexUrl: 'https://doctor-scheduling-system-production.up.railway.app/springbootjf5zc/front/index.html'
         };
     },
-    getProjectName(){
+    getProjectName() {
         return {
             projectName: "医护人员排班系统"
-        } 
+        }
     }
 }
 export default base
